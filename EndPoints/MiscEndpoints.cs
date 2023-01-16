08e788ee7ac3cf20d877564ce9567333e8a6c63f
Kayden Miller <kaydenmiller1@gmail.com>
2023-01-15 17:49:41 -0700

    refactor: clean up Program.cs

---
+++ EndPoints/MiscEndpoints.cs
@@ -0,0 +1,76 @@
+﻿using Miller.MatchMaking.EloRating;
+using StatTracker.DbContexts;
+using StatTracker.Interfaces;
+using StatTracker.Models;
+
+namespace StatTracker.EndPoints;
+
+public class MiscEndpoints : IEndpoint
+{
+    public void DefineServices(IServiceCollection services)
+    {
+        
+    }
+
+    public void DefineEndpoints(WebApplication app)
+    {
+        app.MapGet("/", () => "Hello World!");
+
+        app.MapGet("/TestData", DoStuff);
+
+        app.MapGet("/GetDeck", GetDeck);
+        app.MapPost("/MakeDeck", MakeDeck);
+
+        app.MapGet("/EloStuff/{winner:int}/{loser:int}", EloStuff);
+    }
+    
+    
+    IResult MakeDeck(string deckName, MyDbContext context)
+    {
+        try
+        {
+            // query for player id
+            // context.Decks.Add(new Deck {DeckName = deckName, PlayerId = });
+            context.SaveChanges();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return Results.Problem("Someone messed up...");
+        }
+
+        return Results.Ok();
+    }
+
+    IResult GetDeck(string deckName, MyDbContext context)
+    {
+        try
+        {
+            var stuff = context.Decks.First(x =>
+                x.DeckName == deckName
+            );
+
+            return Results.Ok(stuff);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+
+    IResult EloStuff(int winner, int loser)
+    {
+        return Results.Ok((int)EloRating.CalculateEloExchanged(new EloRating(winner), new EloRating(loser)));
+    }
+
+    IResult DoStuff()
+    {
+        var data = new List<TestData>();
+        data.Add(new TestData("Anje Falkenrath", false, "1000"));
+        data.Add(new TestData("Zurgo Helmsmasher", true, "990"));
+        data.Add(new TestData("Prosper, Tome-Bound", false, "880"));
+
+        return Results.Ok(data);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,19 +1,18 @@
 using Microsoft.EntityFrameworkCore;
-using Miller.MatchMaking.EloRating;
 using StatTracker;
 using StatTracker.DbContexts;
 using StatTracker.Extensions;
-using StatTracker.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointDefinitions(typeof(AssemblyMarker));
 
-var myAllowSpecificOrigins = "_myAllowSpecificOrigins";
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy(myAllowSpecificOrigins,
-        policy => { policy.WithOrigins("http://localhost:4200"); });
+    options.AddDefaultPolicy(policy => policy
+       .AllowAnyHeader()
+       .AllowAnyMethod()
+       .AllowAnyOrigin());
 });
 
 builder.Services.AddEndpointsApiExplorer();
@@ -26,16 +25,8 @@ builder.Services.AddDbContext<MyDbContext>(options =>
 });
 
 var app = builder.Build();
-app.UseCors(myAllowSpecificOrigins);
 
-app.MapGet("/", () => "Hello World!");
-
-app.MapGet("/TestData", DoStuff);
-
-app.MapGet("/GetDeck", GetDeck);
-app.MapPost("/MakeDeck", MakeDeck);
-
-app.MapGet("/EloStuff/{winner:int}/{loser:int}", EloStuff);
+app.UseCors();
 
 app.UseEndpointDefinitions();
 
@@ -43,52 +34,3 @@ app.UseSwagger();
 app.UseSwaggerUI();
 
 app.Run();
-
-IResult MakeDeck(string deckName, MyDbContext context)
-{
-    try
-    {
-        // query for player id
-        // context.Decks.Add(new Deck {DeckName = deckName, PlayerId = });
-        context.SaveChanges();
-    }
-    catch (Exception e)
-    {
-        Console.WriteLine(e);
-        return Results.Problem("Someone messed up...");
-    }
-
-    return Results.Ok();
-}
-
-IResult GetDeck(string deckName, MyDbContext context)
-{
-    try
-    {
-        var stuff = context.Decks.First(x =>
-            x.DeckName == deckName
-        );
-
-        return Results.Ok(stuff);
-    }
-    catch (Exception e)
-    {
-        Console.WriteLine(e);
-        throw;
-    }
-}
-
-IResult EloStuff(int winner, int loser)
-{
-    return Results.Ok((int)EloRating.CalculateEloExchanged(new EloRating(winner), new EloRating(loser)));
-}
-
-IResult DoStuff()
-{
-    var data = new List<TestData>();
-    data.Add(new TestData("Anje Falkenrath", false, "1000"));
-    data.Add(new TestData("Zurgo Helmsmasher", true, "990"));
-    data.Add(new TestData("Prosper, Tome-Bound", false, "880"));
-
-    return Results.Ok(data);
-}
