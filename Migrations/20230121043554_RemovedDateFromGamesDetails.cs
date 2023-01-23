6f8f73c2dcfe5c1fdce250a8eca8647982bb1db3
Tanner Smith <smithjtanner@gmail.com>
2023-01-22 20:05:01 -0700

    oops

---
+++ Migrations/20230121043554_RemovedDateFromGamesDetails.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StatTracker.Migrations
+{
+    public partial class RemovedDateFromGamesDetails : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "GamesDetails");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Date",
+                table: "GamesDetails",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
